344f26390bc9e1185d731100292a22449652826f
AliArzanCode <aliarzanzaidi123@gmail.com>
2026-05-02 06:53:22 +0530

    changes

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  siteUrl:`${process.env.NEXTAUTH_URL}`,
-  generateRobotsTxt: true,
-   robotsTxtOptions: {
-    additionalSitemaps: [
-      `${process.env.NEXTAUTH_URL}/sitemap.xml`,
-    ],
-  },
-};
